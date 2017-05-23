ef862d00e2423cf2ed30c8b840cc3544f1bfe536
murphman300 <jeanlouismurphy@gmail.com>
2017-05-23 14:54:32 -0400

    Added A Selector Button Class

--- SCKBase/SelectorButton.swift
+++ SCKBase/SelectorButton.swift
@@ -11,8 +11,57 @@ import UIKit
 
 public class SelectorButton: UIButton {
     
+    public var currentURLString : String?
     
-
+    open var shouldUseEmptyImage = true
+    
+    private var urlStringForChecking: String?
+    
+    private var emptyImage: UIImage?
+    
+    public init(cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil) {
+        super.init(frame: .zero)
+        contentMode = .scaleAspectFill
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        self.emptyImage = emptyImage
+    }
+    
+    public convenience init(cornerRadius: CGFloat = 0) {
+        self.init(cornerRadius: cornerRadius, emptyImage: nil)
+        isUserInteractionEnabled = true
+    }
+    
+    public convenience init(cornerRadius: CGFloat = 0, tapCallback: @escaping (() ->())) {
+        self.init(cornerRadius: cornerRadius, emptyImage: nil)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public convenience init(cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil, tapCallback: @escaping (() ->())) {
+        self.init(cornerRadius: cornerRadius, emptyImage: emptyImage)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public convenience init(cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil, colorScheme : SpotitColorScheme?, tapCallback: @escaping (() ->())) {
+        self.init(cornerRadius: cornerRadius, emptyImage: emptyImage)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public func handleTap() {
+        tapCallback?()
+    }
+    
+    private var tapCallback: (() -> ())?
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 
