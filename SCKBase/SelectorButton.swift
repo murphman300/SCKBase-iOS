1f73832668a9b22ece10b2fa60e4acb5e821f31c
murphman300 <jeanlouismurphy@gmail.com>
2017-05-23 15:35:06 -0400

    Added Title Init's for SelectorButton

--- SCKBase/SelectorButton.swift
+++ SCKBase/SelectorButton.swift
@@ -27,6 +27,15 @@ public class SelectorButton: UIButton {
         self.emptyImage = emptyImage
     }
     
+    public init(title: String?, cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil) {
+        super.init(frame: .zero)
+        contentMode = .scaleAspectFill
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        setTitle(title, for: .normal)
+        self.emptyImage = emptyImage
+    }
+    
     public convenience init(cornerRadius: CGFloat = 0) {
         self.init(cornerRadius: cornerRadius, emptyImage: nil)
         isUserInteractionEnabled = true
@@ -48,6 +57,32 @@ public class SelectorButton: UIButton {
     
     public convenience init(cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil, colorScheme : SpotitColorScheme?, tapCallback: @escaping (() ->())) {
         self.init(cornerRadius: cornerRadius, emptyImage: emptyImage)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public convenience init(title: String?, cornerRadius: CGFloat = 0) {
+        self.init(title: title, cornerRadius: cornerRadius, emptyImage: nil)
+        isUserInteractionEnabled = true
+    }
+    
+    public convenience init(title: String?, cornerRadius: CGFloat = 0, tapCallback: @escaping (() ->())) {
+        self.init(title: title, cornerRadius: cornerRadius, emptyImage: nil)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public convenience init(title: String?, cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil, tapCallback: @escaping (() ->())) {
+        self.init(title: title, cornerRadius: cornerRadius, emptyImage: emptyImage)
+        self.tapCallback = tapCallback
+        isUserInteractionEnabled = true
+        addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
+    }
+    
+    public convenience init(title: String?, cornerRadius: CGFloat = 0, emptyImage: UIImage? = nil, colorScheme : SpotitColorScheme?, tapCallback: @escaping (() ->())) {
+        self.init(title: title, cornerRadius: cornerRadius, emptyImage: emptyImage)
         self.tapCallback = tapCallback
         isUserInteractionEnabled = true
         addTarget(self, action: #selector(handleTap), for: UIControlEvents.touchUpInside)
